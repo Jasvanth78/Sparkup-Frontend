c6621434780e8ecc586ae1cccc8c2a5f372c30fc
Jasvanth S <mailtojasvanth88-coder@users.noreply.github.com>
2026-01-22 15:54:14 +0530

    fix deploy bug

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,12 @@ import App from './App.jsx'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { ThemeProvider } from './Context/ThemeContext';
-import { HashRouter } from 'react-router-dom';
+
 
 createRoot(document.getElementById('root')).render(
  
     <ThemeProvider>
-      <HashRouter>
       <App />
-      </HashRouter>
       <ToastContainer />
     </ThemeProvider>
 
